0f1f9aa28ac366543517b9b9ffe4382b45d35f32
Tanya Rajput <tanyarajput7800@gmail.com>
2026-09-02 19:45:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-//console.log(Document)
-//console.log(document.body)
-//window.console.log("tanya")
-//console.log(window)
-
-// THERE are mainly 4 things jisse hum dom ke html nodes ko select kr sakte h 
-
-
-// first way using className
-// const heading = document.getElementsByClassName("heading")
-// console.log("class name")
-// console.log(heading)
-
-
-// // second way
-
-// const subheading = document.getElementById("SUBHEADING")
-// console.log(subheading)
-//console.dir(subheading)
-
-
-
-// third way
-
-// document.querySelector("li")
-//document.querySelectorAll("li")
-
-
-// innerHTML, innerText , textContent
-
-//document.querySelector("one")
-//const newatri = document.querySelector("a")
-
-
 const num1 = document.querySelector('.num1');
 const num2 = document.querySelector('.num2');
 const result = document.querySelector('.result');
